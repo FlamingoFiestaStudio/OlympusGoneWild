cab1fb80dc5dce8d0748e72f0df238b49066e43d
Goncalo_P <1222134@isep.ipp.pt>
2023-09-10 11:16:19 +0100

    Player movement
    - Modified the project so that players can move the character with both "wasd" and arrows.
    - Added an asset to test the player movement code.
    - Coded the script for player movement.

---
+++ Player/Player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+var speed = 200
+@onready var anim = get_node("AnimatedSprite2D")
+
+func _ready():
+	anim.play("Idle")
+
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		anim.play("Running")
+	else:
+		anim.play("Idle")
+	position += velocity * delta
+	
