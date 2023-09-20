8a1b24b861ef14ead4c3b7c1dcf2e675afe59136
Goncalo_P <1222134@isep.ipp.pt>
2023-09-20 11:40:20 +0100

    More Suggested Changes
    - More readble code
    - More generic name for the inputs
    - Added the code to flip the player sprite

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,24 +1,31 @@
 extends CharacterBody2D
 
 
-const speed = 200
+const SPEED = 200
 @onready var anim = get_node("AnimatedSprite2D")
 
 func _ready():
 	anim.play("Idle")
 
-func _process(delta):
+func _physics_process(delta):
 	var velocity = Vector2()
-	if Input.is_action_pressed("move_right"):
+	
+	if Input.is_action_pressed("right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+		anim.flip_h = false
+		
+	if Input.is_action_pressed("left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+		anim.flip_h = true
+		
+	if Input.is_action_pressed("down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+		
+	if Input.is_action_pressed("up"):
 		velocity.y -= 1
+		
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		velocity = velocity.normalized() * SPEED
 		anim.play("Running")
 	else:
 		anim.play("Idle")
