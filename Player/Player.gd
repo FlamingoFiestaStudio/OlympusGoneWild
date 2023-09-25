df94ab2ad6070873ffb5bf8ff39c2616eeba8145
Goncalo_P <1222134@isep.ipp.pt>
2023-09-25 12:49:56 +0100

    Updated Player.gd
    - Updated script for player movement with functions "_process" and
    "_physics_process" properly implemented.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,33 +1,39 @@
 extends CharacterBody2D
 
 
+var direction = Vector2()
 const SPEED = 200
 @onready var anim = get_node("AnimatedSprite2D")
+
 
 func _ready():
 	anim.play("Idle")
 
-func _physics_process(delta):
-	var velocity = Vector2()
+
+func _process(delta):
+	direction = Vector2() #Reset direction
 	
 	if Input.is_action_pressed("right"):
-		velocity.x += 1
+		direction.x += 1
 		anim.flip_h = false
 		
 	if Input.is_action_pressed("left"):
-		velocity.x -= 1
+		direction.x -= 1
 		anim.flip_h = true
 		
 	if Input.is_action_pressed("down"):
-		velocity.y += 1
+		direction.y += 1
 		
 	if Input.is_action_pressed("up"):
-		velocity.y -= 1
+		direction.y -= 1
 		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
+	if direction.length() > 0:
+		direction = direction.normalized() * SPEED
 		anim.play("Running")
 	else:
 		anim.play("Idle")
-	position += velocity * delta
+
+
+func _physics_process(delta):
+	position += direction * delta
 	
