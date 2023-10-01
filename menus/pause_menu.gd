5487430adad5624c46793899054e66cd7fd280a0
Gabriel Sousa <116306408+Gabriel-Sous-a@users.noreply.github.com>
2023-10-01 23:33:41 +0100

    Added logic to pause menu (#61)
    * Added logic to pause menu
    
    * Update pause_menu.gd
    
    * Update pause_menu.gd

--- menus/pause_menu.gd
+++ menus/pause_menu.gd
@@ -1,11 +1,25 @@
 extends Control
 
+var game_paused : bool = false : get = _get_game_paused, set = _set_game_paused
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hide()
 
+func _get_game_paused():
+	return game_paused
+	
+func _set_game_paused(new_state: bool):
+	game_paused = new_state
+	get_tree().paused = game_paused
+	show() if game_paused else hide()
+
+func _input(event : InputEvent):
+	if(event.is_action_pressed("ui_cancel")):
+		game_paused = !game_paused
+
+func _on_resume_button_pressed():
+	game_paused = false
 
 func _on_quit_button_pressed():
-	pass
-	
+	game_paused = false
+	get_tree().change_scene_to_file("res://menus/menu.tscn")
