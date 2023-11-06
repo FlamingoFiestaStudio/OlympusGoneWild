65f1c43a2d2d6a4c4441aa40c25152f5a9ac3fc3
Gabriel Sousa <gabriel2001@live.com.pt>
2023-11-06 23:16:58 +0000

    fix: refactor code

--- enemies/Enemy.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	pass
--- main/level_1.gd
+++ main/level_1.gd
@@ -15,13 +15,13 @@ func _ready():
 	camera.limit_bottom = map_rect.end.y * tile_size.y
 	
 
-func _on_timer_timeout():
+func _on_enemy_spawner_timer_timeout() -> void:
 	var enemy_instance = enemy_scene.instantiate()
 	var tile_size = tile_set.tile_size
 	var map_rect = tilemap.get_used_rect()
 	var buffer = 30
-	var spawn_area_x = (map_rect.size.x * tile_size.x)/2 + buffer
-	var spawn_area_y = (map_rect.size.y * tile_size.y)/2 + buffer
+	var spawn_area_x = (map_rect.size.x * tile_size.x) / 2 + buffer
+	var spawn_area_y = (map_rect.size.y * tile_size.y) / 2 + buffer
 	var possible_spawn_x = [spawn_area_x, -spawn_area_x]
 	var possible_spawn_y = [spawn_area_y, -spawn_area_y]
 	
@@ -29,10 +29,10 @@ func _on_timer_timeout():
 	var random_y = 0
 	if randf() < 0.5:
 		random_x = randf_range(spawn_area_x, -spawn_area_x)
-		random_y = possible_spawn_y[randi() % 2]
+		random_y = possible_spawn_y.pick_random()
 	else:
 		random_y = randf_range(spawn_area_y, -spawn_area_y)
-		random_x = possible_spawn_x[randi() % 2]
+		random_x = possible_spawn_x.pick_random()
 
-	enemy_instance.transform = Transform2D(0, Vector2(random_x, random_y))
+	enemy_instance.position = Vector2(random_x, random_y)
 	add_child(enemy_instance)
