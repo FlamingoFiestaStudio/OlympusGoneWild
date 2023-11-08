26d6afb8788b0d7892e44d0d4ce83def37488362
Flávio Silva <55815265+201flaviosilva@users.noreply.github.com>
2023-11-08 18:19:10 +0000

    Removed unused variables and nodes (#124)
    refactor: removed direction not

--- player/Player.gd
+++ player/Player.gd
@@ -15,27 +15,27 @@ func _process(_delta) -> void:
 	_control()
 
 func _control() -> void:
-	direction = Vector2() #Reset direction
+	direction = Vector2() # Reset direction
 	
 	if Input.is_action_pressed("right"):
 		direction.x += 1
 		anim.flip_h = false
-		
+	
 	if Input.is_action_pressed("left"):
 		direction.x -= 1
 		anim.flip_h = true
-		
+	
 	if Input.is_action_pressed("down"):
 		direction.y += 1
-		
+	
 	if Input.is_action_pressed("up"):
 		direction.y -= 1
-		
+	
 	if direction.length() > 0:
 		anim.play("Running")
 	else:
 		anim.play("Idle")
-		
+	
 	velocity = direction * SPEED
 
 func _physics_process(_delta) -> void:
