4c472ab0cc1d69aa7c5fc797285122472f909667
Flávio Silva <55815265+201flaviosilva@users.noreply.github.com>
2023-11-25 09:33:34 +0000

    Enemy gets hit (#138)
    * fix: fix some console warning
    
    * feat(Enemy): Enemy attack system setup
    
    Co-Authored-By: Gabriel Sousa <116306408+Gabriel-Sous-a@users.noreply.github.com>
    
    * Update Enemy.gd
    
    * fix(Enemy): renamed variable
    
    * feat(#64): Player bullets hit enemy
    
    On player bullets hit the enemy the enemy lose health, if gets less than 0 the enemy dies and the player win 1 score point
    
    * Update Enemy.gd
    
    * Update Enemy.gd
    
    ---------
    
    Co-authored-by: Gabriel Sousa <116306408+Gabriel-Sous-a@users.noreply.github.com>

--- menus/pause_menu.gd
+++ menus/pause_menu.gd
@@ -1,25 +1,25 @@
 extends Control
 
-var game_paused : bool = false : get = _get_game_paused, set = _set_game_paused
+var game_paused: bool = false : get = _get_game_paused, set = _set_game_paused
 
-func _ready():
+func _ready() -> void:
 	hide()
 
-func _get_game_paused():
+func _get_game_paused() -> bool:
 	return game_paused
 	
-func _set_game_paused(new_state: bool):
+func _set_game_paused(new_state: bool) -> void:
 	game_paused = new_state
 	get_tree().paused = game_paused
 	show() if game_paused else hide()
 
-func _input(event : InputEvent):
+func _input(event: InputEvent)  -> void:
 	if(event.is_action_pressed("ui_cancel")):
 		game_paused = !game_paused
 
-func _on_resume_button_pressed():
+func _on_resume_button_pressed()  -> void:
 	game_paused = false
 
-func _on_quit_button_pressed():
+func _on_quit_button_pressed()  -> void:
 	game_paused = false
 	get_tree().change_scene_to_file("res://menus/menu.tscn")
